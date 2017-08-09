260af35ddde9fe285b388da5b13e6a468349b178
VkcxR2ExcFhWblU9 <30832553+VkcxR2ExcFhWblU9@users.noreply.github.com>
2017-08-09 18:27:04 +0100

    Update server.py

--- server.py
+++ server.py
@@ -62,8 +62,8 @@ def x(path):
     data['data'] = dt
     data['raw'] = path[2:]
     fb.patch(datetime.datetime.now().strftime("/out/%Y/%m/%d/%H/%M/%S/%f/"),data)
-    fb.patch('/input/done/','true');
-    return fb.get('in/'+str(fb.get('/input/cmd/',None))+'/cmd',None)
+    #fb.patch('/input/done/','true');
+    return fb.get('/input/cmd/',None)#fb.get('in/'+str(fb.get('/input/cmd/',None))+'/cmd',None)
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
